74a915271bb35381849150fca5abfcb51a8750ec
danyanga <anyanga14@gmail.com>
2023-06-09 17:23:15 -0600

    created the login and the registration  form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        
+        <LoginForm/>
       </div>
       
     </>
