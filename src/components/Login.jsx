17fd9d7ed904db6dc6eb98a9257d1f92eb4548a5
danyanga <anyanga14@gmail.com>
2023-06-09 10:09:00 -0600

    deleted the unnecessary codes e.g. the vite and react logo codes

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+  return (
+
+    <div className="cover">
+      <h1>LOGIN</h1>
+      <input type="text" placeholder="username" ></input>
+      <input type="password" placeholder="password"></input>
+  
+    
+    </div>
+  )
+}
+export default Login
