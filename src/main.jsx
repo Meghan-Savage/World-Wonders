fe65370fbde59e0f0f136c7ee05a06c239b4938f
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-06-12 11:38:27 -0600

    - fixed ProductProvider import into main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FirebaseProvider from "./firebase/provider.jsx";
 import { AuthProvider } from "./firebase/authentication.jsx";
+import ProductProvider from "./context/ProductContext/ProductContext.jsx"
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -9,10 +10,12 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FirebaseProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+      <AuthProvider>  
+        <ProductProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ProductProvider>
       </AuthProvider>
     </FirebaseProvider>
   </React.StrictMode>
