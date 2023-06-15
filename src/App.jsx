c0b24446c421642629aa62656f1530e7b1c31ec4
danyanga <anyanga14@gmail.com>
2023-06-15 12:16:54 -0600

    corrected few styling on the loginform

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home.jsx";
 import Products from "./pages/allProducts/AllProducts.jsx";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm"
 
 function App() {
   return (
     <Routes>
       <Route path="/products" element={<Products />} />
       <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<LoginForm />} />
+      <Route path="/signup" element={<RegistrationForm />} />
     </Routes>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,32 +7,32 @@ export default function LoginForm() {
   
   return (
 
-    <div className="bg-slate-200 px-60 py-40 rounded-3xl border-2 border-gray-200">
-    <h1 className="text-6xl text-blue-700 font-semibold">Login</h1>
+    <div className="bg-slate-200 px-96 py-2 rounded-3xl border-2 border-gray-200">
+    <h1 className="text-4xl text-blue-700 font-semibold">Login</h1>
     <p className="font-bold text-lg text-gray-600 mt-4">
       Welcome back! Please enter your login details.</p>
-    <div className="mt-8">
+    <div className="mt-4">
       <div>
-          <label className="text-4xl font-medium font-bold">Email</label>
+          <label className="text-1xl font-medium font-bold">Email</label>
           <input className="w-full border-2 border-gray-400 rounded-xl p-4 mt-2 bg-transparent" 
           type="text" placeholder="Enter your email"/>
       </div>
     </div>
 
     <div>
-          <label className="text-3xl font-medium">Password</label>
+          <label className="text-1xl font-medium">Password</label>
           <input className="w-full border-2 border-gray-400 rounded-xl p-4 mt-2 bg-transparent" 
           type="password" placeholder="Enter your password"/>
       </div>
       
       <div className="mt-4 flex flex-col gap-y-4">
           <button className=
-          "hover: active: scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-700 border-gray-400 big-violet-500 text-blue text-4xl font-bold" 
+          "hover: active: scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-700 border-gray-400 big-violet-500 text-blue text-2xl font-bold" 
           type="submit">Sign in</button>
 
           <div className="mt-2 grid grid-cols-3 items-center text-red-900">
               <hr className="border-gray-800" />
-              <p className="text-center  font-semibold text-2xl ">Or</p>
+              <p className="text-center  font-semibold text-1xl ">Or</p>
               <hr className="border-gray-800" />
           </div>
         
@@ -45,7 +45,7 @@ export default function LoginForm() {
       </div>
       <div className="mt-4 flex justify-center items-center">
           <p className="font-bold text-1xl">Don't have an account?</p>
-          <button className="text-violet-500 text-2xl font-bold ml-2"></button>
+          <button className="text-violet-500 text-1xl font-bold ml-2">Sign up</button>
       </div>
   </div>
     
