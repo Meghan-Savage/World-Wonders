2dabd6554281510790748ed3c18578b7c8630d1b
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-06-17 16:54:35 -0600

    - fixed some routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import Products from "./pages/allProducts/AllProducts.jsx";
+import LoginForm from "./Components/LoginForm.jsx";
+import RegistrationForm from "./Components/RegistrationForm.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/products" element={<Products />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<RegistrationForm />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
 }
 
 export default App;
-
-
