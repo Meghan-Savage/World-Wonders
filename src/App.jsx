bbf5d1b79bae40e6eedeb065e4153c00878cfe11
Meghan-Savage <MSavage1@protonmail.com>
2023-06-19 21:10:45 -0600

    fixed deleted and removed code

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home.jsx";
 import Products from "./pages/allProducts/AllProducts.jsx";
 import ProductDetails from "./pages/productDetails/ProductDetails.jsx";
 import SideBar from "./Components/sideBar/SideBar.jsx";
 import ViewCart from "./pages/viewCart/ViewCart.jsx";
 import LoginForm from "./Components/LoginForm.jsx";
 import RegistrationForm from "./Components/RegistrationForm.jsx";
-
+import AdminShowProducts from "./components/AdminShowProducts";
+import CreateProductForm from "./components/CreateProductForm";
 
 function App() {
   return (
@@ -19,10 +19,10 @@ function App() {
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<ViewCart />} />
         <Route path="/signin" element={<LoginForm />} />
-      <Route path="/signup" element={<RegistrationForm />} />
-      <Route path="/" element={<Home />} />
-        
-    </Routes>
+        <Route path="/signup" element={<RegistrationForm />} />
+        <Route path="/admin-products" element={<AdminShowProducts />} />
+        <Route path="/create-product" element={<CreateProductForm />} />
+      </Routes>
       <SideBar />
     </>
   );
