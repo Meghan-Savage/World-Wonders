d97d62f63a68da061a80c900d8770982fef36b69
danyanga <anyanga14@gmail.com>
2023-06-30 16:27:02 -0600

    modified the navbar to show the flag whena country is selected and removed the unnecessary.

--- src/Components/TopPage.jsx
+++ src/Components/TopPage.jsx
@@ -1,91 +1,68 @@
-import React from "react";
-import {
-  FaShoppingCart,
-  FaUser,
-  FaSearch,
-  FaSignInAlt,
-} from "react-icons/fa";
+import React, { useState } from "react";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import countryList from "country-list";
+import ReactCountryFlag from "react-country-flag";
 import UserDropdown from "./UserDropdown";
 
-
-
-
-const menuItems = [
-  { text: "Home", to: "/" },
-  { text: "Products", to: "/products" },
-  {
-    text: "Profile",
-    to: "/signin",
-    icon: <FaUser className="mr-1" />,
-    
-    subItems: [
-      {
-        text: "Sign In",
-        to: "/signin",
-        icon: <FaSignInAlt className="ml-1 text-orange-200" />,
-      },
-      { text: "Sign Up", to: "/signup" },
-    ],
-  },
-  { text: "Admin Products", to: "/admin-products" },
-  { text: "Sign In", to: "/signin" },
-  { text: "Create Product", to: "/create-product" },
-];
+const countries = countryList.getData();
 
 const Navbar = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleSelectCountry = (countryCode) => {
+    setSelectedCountry(countryCode);
+  };
+
   return (
     <nav className="bg-gray-900 py-4">
       <ul className="flex justify-between container mx-auto px-8">
-        {menuItems.map((item, index) => (
-          <li key={index}>
-            {item.subItems ? (
-              <div className="relative">
-                <Link
-                  to={item.to}
-                  className="text-orange-200 hover:text-gray-400"
-                >
-                  {item.text}
-                </Link>
-                <div className="absolute z-10 bg-white hidden">
-                  {item.subItems.map((subItem, subIndex) => (
-                    <Link
-                      key={subIndex}
-                      to={subItem.to}
-                      className="block px-4 py-2 text-orange-200 hover:text-gray-400"
-                    >
-                      {subItem.icon}
-                      {subItem.text}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            ) : (
-              <Link
-                to={item.to}
-                className="text-orange-200 hover:text-gray-400 flex items-center"
-              >
-                {item.icon}
-                {item.text}
-              </Link>
-            )}
-          </li>
-        ))}
-        <li className="navbar-right">
-          <div className="flex items-center">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Type here..."
-                className="border rounded-l px-2 py-1 pl-8 rounded-r"
-              />
-              <div className="absolute top-0 left-0 mt-2 ml-2">
-                <FaSearch className="text-gray-600" />
-              </div>
-            </div>
-          </div>
+        <li>
+          <Link
+            to="/"
+            className="text-orange-200 hover:text-gray-400 block sm:inline-block"
+          >
+            Home
+          </Link>
         </li>
-        <li className="navbar-right flex items-center">
+        <li>
+          <Link
+            to="/products"
+            className="text-orange-200 hover:text-gray-400 block sm:inline-block"
+          >
+            Products
+          </Link>
+        </li>
+        <li className="flex items-center space-x-2">
+          <select
+            onChange={(e) => handleSelectCountry(e.target.value)}
+            className="text-orange-200 bg-gray-900 border border-gray-400 rounded py-1 px-2"
+          >
+            <option value="">Select Country</option>
+            {countries.map((country) => (
+              <option key={country.code} value={country.code}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+          {selectedCountry && (
+            <ReactCountryFlag
+              countryCode={selectedCountry}
+              svg
+              className="ml-2 w-8 h-8"
+            />
+          )}
+        </li>
+        <li>
+          <Link
+            to="/cart"
+            className="text-orange-200 hover:text-gray-400 flex items-center"
+          >
+            <FaShoppingCart className="mr-1" />
+            Cart
+          </Link>
+        </li>
+        <li className="flex items-center">
           <UserDropdown />
         </li>
       </ul>
--- src/Components/UserDropdown.jsx
+++ src/Components/UserDropdown.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaUser, FaSignOutAlt, FaCaretDown } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const UserAccountDropdown = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+      // Check if the clicked element is outside the dropdown
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+
+    // Add event listener for page refresh/close
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // Add event listener for click anywhere on the document
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      // Remove event listeners when the component unmounts
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
+
+  const handleBeforeUnload = () => {
+    // Clear the dropdown state before the page refreshes/closes
+    setShowDropdown(false);
+  };
+
   // logout logic
   const handleLogout = () => {
     // Clear any user-specific data from local storage, cookies, or other storage mechanisms
@@ -21,29 +49,61 @@ const UserAccountDropdown = () => {
     setShowDropdown(false);
   };
 
+  const handlePostProduct = () => {
+    // Navigate to the CreateProduct component
+    navigate("/create-product");
+
+    setShowDropdown(false);
+  };
+
+  const handleLogin = () => {
+    // Navigate to the CreateProduct component
+    navigate("/signin");
+
+    setShowDropdown(false);
+  };
+
   return (
-    <div className="relative z-50">
+    <div className="relative z-50" ref={dropdownRef}>
       <button
         onClick={toggleDropdown}
         className="flex items-center text-orange-200 hover:text-gray-400 focus:outline-none"
       >
-        <FaUser className="mr-1" />
-        <FaCaretDown className="text-xs" />
+        <FaUser className="mr-1" style={{ fontSize: "24px" }} />
+        <span className="hidden sm:inline">Profile</span>
+        <FaCaretDown className="text-xs ml-1" />
       </button>
       {showDropdown && (
         <div className="absolute right-0 mt-2 bg-gray-200 border rounded border-gray-800 w-60">
-         
           <button
-            onClick={() => console.log("My Profile clicked")}
+          onClick={handleLogin}
+            
             className="block px-4 py-2 text-black hover:bg-orange-200 w-full text-left"
           >
-            My Profile
+            Login
           </button>
           <button
-            onClick={() => console.log("Edit Profile clicked")}
             className="block px-4 py-2 text-black hover:bg-orange-200 w-full text-left"
           >
-            Edit Profile
+            Your Orders
+          </button>
+          <button
+            
+            className="block px-4 py-2 text-black hover:bg-orange-200 w-full text-left"
+          >
+            Order Shipping
+          </button>
+          <button
+            onClick={handlePostProduct}
+            className="block px-4 py-2 text-black hover:bg-orange-200 w-full text-left"
+          >
+            Post Product(s)
+          </button>
+          <button
+            
+            className="block px-4 py-2 text-black hover:bg-orange-200 w-full text-left"
+          >
+            Order Management
           </button>
           <button
             onClick={handleLogout}
