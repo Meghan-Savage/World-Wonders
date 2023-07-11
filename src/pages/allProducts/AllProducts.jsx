a7389d20434eed12b3c624bd09eddc1bcffb707f
Meghan-Savage <MSavage1@protonmail.com>
2023-07-11 01:48:12 -0600

    updated card styles to center on page, style fix on product details

--- src/pages/allProducts/AllProducts.jsx
+++ src/pages/allProducts/AllProducts.jsx
@@ -45,10 +45,10 @@ const Products = () => {
     : products;
 
   return (
-    <>
-      <section className="py-16">
+    <div className="bg-gradient-to-t from-orange-400 to-orange-100">
+      <section className="py-16 flex">
         <div className="mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[2rem] max-w-sm mx-auto md:max-w-none md:mx-0">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[6rem] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts &&
               filteredProducts.map((product) => (
                 <Product product={product} key={product.id} />
@@ -60,7 +60,7 @@ const Products = () => {
       </section>
 
       <Outlet />
-    </>
+    </div>
   );
 };
 
