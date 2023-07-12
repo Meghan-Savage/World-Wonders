5091e154761251deaf29f6a99a276cf26fc197ca
danyanga <anyanga14@gmail.com>
2023-07-12 13:39:20 -0600

    adjusted the size of the select country button

--- src/Components/TopPage.jsx
+++ src/Components/TopPage.jsx
@@ -84,90 +84,88 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gray-900 py-4">
-      <ul className="flex justify-between items-center container mx-auto px-8">
-        <li>
-          <Link
-            to="/"
-            className="text-orange-200 hover:text-gray-400 block sm:inline-block"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/products"
-            className="text-orange-200 hover:text-gray-400 block sm:inline-block"
-          >
-            Products
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <select
-            onChange={(e) => handleSelectCountry(e.target.value)}
-            className="text-orange-200 bg-gray-900 border border-gray-400 rounded py-1 px-2"
-          >
-            <option value="">Select Country</option>
-            {countries.map((country) => (
-              <option key={country.code} value={country.name}>
-                {country.name}
-              </option>
-            ))}
-          </select>
-          {selectedCountry && (
-            <ReactCountryFlag
-              countryCode={selectedCountry}
-              svg
-              className="ml-2 w-8 h-8"
-            />
-          )}
-        </li>
-        <li>
-          <Link
-            className="text-orange-200 hover:text-gray-400 block sm:inline-block"
-            to="/cart"
-          >
-            <ShoppingCart />
-          </Link>
-        </li>
-        <li className="relative">
-          {isLoggedIn && (
-            <button
-              onClick={toggleProfileDropdown}
-              className="text-orange-200 hover:text-gray-400 block sm:inline-block"
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <Link
+              to="/"
+              className="text-orange-200 hover:text-gray-400 block sm:inline-block mr-16"
             >
-              {userEmail}
-              <svg
-                className={`${
-                  showProfileDropdown ? "transform rotate-180" : ""
-                } inline-block ml-1 h-4 w-4`}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                ref={dropdownRef}
+              Home
+            </Link>
+            <Link
+              to="/products"
+              className="text-orange-200 hover:text-gray-400 block sm:inline-block mr-4"
+            >
+              Products
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <select
+              onChange={(e) => handleSelectCountry(e.target.value)}
+              className="text-orange-200 bg-gray-900 border border-gray-400 rounded py-1 px-24 mr-2"
+            >
+              <option value="">Select Country</option>
+              {countries.map((country) => (
+                <option key={country.code} value={country.name}>
+                  {country.name}
+                </option>
+              ))}
+            </select>
+            {selectedCountry && (
+              <ReactCountryFlag
+                countryCode={selectedCountry}
+                svg
+                className="ml-2 w-8 h-8"
+              />
+            )}
+          </div>
+          <div className="flex items-center">
+            <Link
+              className="text-orange-200 hover:text-gray-400 mr-8"
+              to="/cart"
+            >
+              <ShoppingCart />
+            </Link>
+            {isLoggedIn && (
+              <button
+                onClick={toggleProfileDropdown}
+                className="text-orange-200 hover:text-gray-400 focus:outline-none"
               >
-                <polyline points="6 9 12 15 18 9" />
-              </svg>
-            </button>
-          )}
-          {isLoggedIn ? (
-            <button
-              onClick={handleLogout}
-              className="text-orange-200 hover:text-gray-400 block sm:inline-block"
-            >
-              Logout
-            </button>
-          ) : (
-            <button
-              onClick={toggleLoginForm}
-              className="text-orange-200 hover:text-gray-400 block sm:inline-block"
-            >
-              Login
-            </button>
-          )}
+                {userEmail}
+                <svg
+                  className={`${
+                    showProfileDropdown ? "transform rotate-180" : ""
+                  } inline-block ml-1 h-4 w-4`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  ref={dropdownRef}
+                >
+                  <polyline points="6 9 12 15 18 9" />
+                </svg>
+              </button>
+            )}
+            {isLoggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="text-orange-200 hover:text-gray-400 ml-4"
+              >
+                Logout
+              </button>
+            ) : (
+              <button
+                onClick={toggleLoginForm}
+                className="text-orange-200 hover:text-gray-400 ml-4"
+              >
+                Login
+              </button>
+            )}
+          </div>
           {showProfileDropdown && (
             <div className="absolute right-0 mt-2 w-48 bg-gray-800 border border-gray-400 rounded py-2 z-10">
               <button
@@ -184,14 +182,13 @@ const Navbar = () => {
               </button>
             </div>
           )}
-        </li>
-      </ul>
-
-      {showLoginForm && !isLoggedIn && (
-        <div className="container mx-auto px-8 mt-4">
-          <LoginForm onLogin={handleLogin} />
         </div>
-      )}
+        {showLoginForm && !isLoggedIn && (
+          <div className="container mx-auto px-8 mt-4">
+            <LoginForm onLogin={handleLogin} />
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
