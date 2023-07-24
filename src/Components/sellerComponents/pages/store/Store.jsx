35b0e6d910bc955df2d7f9e35cc5c52aece470f6
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-24 14:39:52 -0600

    - added logic to check if forms have been filled

--- src/Components/sellerComponents/pages/store/Store.jsx
+++ src/Components/sellerComponents/pages/store/Store.jsx
@@ -88,6 +88,17 @@ const Store = () => {
     }
   };
 
+  const isFormValid = () => {
+    return (
+      mainImage !== null &&
+      title.trim() !== "" &&
+      description.trim() !== "" &&
+      country.trim() !== "" &&
+      price.trim() !== "" &&
+      quantity.trim() !== ""
+    );
+  };
+
   return (
     <div className="max-w-full mx-4 py-6 sm:mx-auto sm:px-6 lg:px-8 bg-gray-100">
       <PhotosCard
@@ -113,22 +124,26 @@ const Store = () => {
       <div className="mr-8 flex justify-end text-2xl">
         <button
           className="px-8 py-2 bg-gray-900 text-orange-200 font-semibold rounded-lg hover:bg-gray-700 focus:outline-none"
-          onClick={() =>
-            handlePostProduct(
-              db,
-              mainImage,
-              additionalImages,
-              {
-                title,
-                description,
-                country,
-              },
-              {
-                price,
-                quantity,
-              }
-            )
-          }
+          onClick={() => {
+            if (isFormValid()) {
+              handlePostProduct(
+                db,
+                mainImage,
+                additionalImages,
+                {
+                  title,
+                  description,
+                  country,
+                },
+                {
+                  price,
+                  quantity,
+                }
+              );
+            } else {
+              alert("Please fill in all required fields.");
+            }
+          }}
         >
           Post Product
         </button>
