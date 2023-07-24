4aeb4acd0bd124443ee1384974a651e45806e84d
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-23 19:35:14 -0600

    - added sidebar for mobile on dashboard

--- src/Components/sellerComponents/dashboard/dashboard.jsx
+++ src/Components/sellerComponents/dashboard/dashboard.jsx
@@ -13,6 +13,8 @@ import Store from "../pages/store/Store";
 import HomeSeller from "../../../pages/homeSeller/HomeSeller";
 
 function Dashboard() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   const menuItems = [
     { name: "Home", path: "/seller", icon: AiOutlineHome },
     {
@@ -28,26 +30,39 @@ function Dashboard() {
     { name: "Store", path: "/seller/dashboard/store", icon: AiOutlineShop },
   ];
 
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="h-screen flex">
-      <div className="w-32 bg-gray-800 text-orange-200">
-        <div className="p-4 flex items-center justify-center">
+      {/* Collapsible Sidebar */}
+      <div
+        className={`${
+          isSidebarOpen ? "w-32" : "w-16"
+        } bg-gray-800 text-orange-200 transition-all ease-in-out overflow-hidden`}
+      >
+        <div
+          className="p-4 flex items-center justify-center cursor-pointer"
+          onClick={handleSidebarToggle}
+        >
           <AiOutlineDashboard className="h-8 w-8 mr-2" />
-          <h1 className="text-xl font-bold">Dashboard</h1>
+          {isSidebarOpen && <h1 className="text-xl font-bold">Dashboard</h1>}
         </div>
         <hr className="border-orange-200" />
         <ul className="space-y-2">
-          {menuItems.map((item) => (
-            <li key={item.name}>
-              <Link
-                to={item.path}
-                className="flex items-center p-4 hover:bg-gray-700"
-              >
-                <item.icon className="h-6 w-6 mr-2" />
-                <span>{item.name}</span>
-              </Link>
-            </li>
-          ))}
+          {isSidebarOpen &&
+            menuItems.map((item) => (
+              <li key={item.name}>
+                <Link
+                  to={item.path}
+                  className="flex items-center p-4 hover:bg-gray-700"
+                >
+                  <item.icon className="h-6 w-6 mr-2" />
+                  <span>{item.name}</span>
+                </Link>
+              </li>
+            ))}
         </ul>
       </div>
       <div className="flex-1 bg-gray-100 p-4 overflow-auto">
